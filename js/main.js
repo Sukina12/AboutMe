0593f05a239a3939d2239ad4afe67589b61a945a
MURADALSHORMAN <MURADALSHORMAN@GMAIL.COM>
2021-03-17 14:59:56 +0200

    change the code to the functions

--- js/main.js
+++ js/main.js
@@ -4,25 +4,30 @@ let score = 0;
 let name = prompt('Please Enter Your Name' );
 alert ('Welcome  ' + name + '  In My WebPage & I hope to answer These Questions with yes or no');
 
-let Q1 = prompt ('Do I own a laptop?').toLocaleLowerCase()
- 
-if (Q1 === 'yes' || Q1==='y')
+// eslint-disable-next-line no-unused-vars
+function myLaptop(){
+  let Q1 = prompt ('Do I own a laptop?').toLocaleLowerCase();
+
+  if (Q1 === 'yes' || Q1==='y')
   {
-      console.log('Correct I own a LapTop');
-      alert ('Correct answer I own a LapTop');
-      score++ ;
+    console.log('Correct I own a LapTop');
+    alert ('Correct answer I own a LapTop');
+    score++ ;
   }
   else
   {
     console.log('InCorrect answer I own a LapTop');
     alert ('InCorrect answer I own a LapTop');
   }
+}
 
-let Q2 = prompt('Can I ride a bike؟').toLocaleLowerCase()
-if  (Q2 === 'yes' || Q2==='y')
+// eslint-disable-next-line no-unused-vars
+function myBike(){
+  let Q2 = prompt('Can I ride a bike؟').toLocaleLowerCase()
+  if (Q2 === 'yes' || Q2==='y')
   {
-      console.log('Incorrect answer I cant ride a bike');
-      alert ('Incorrect answer I cant ride a bike');
+    console.log('Incorrect answer I cant ride a bike');
+    alert ('Incorrect answer I cant ride a bike');
   }
   else
   {
@@ -30,12 +35,14 @@ if  (Q2 === 'yes' || Q2==='y')
     alert ('correct answer I cant ride a bike');
     score++ ;
   }
+}
 
-  let Q3 = prompt ('Do I like shawarma?').toLocaleLowerCase()
-  if  (Q3 === 'yes' || Q3==='y')
-  {
-      console.log('correct answer I like shawarma');
-      alert ('correct answer I like shawarma');
+function myShawrma(){
+let Q3 = prompt ('Do I like shawarma?').toLocaleLowerCase()
+if (Q3 === 'yes' || Q3==='y')
+{
+  console.log('correct answer I like shawarma');
+  alert ('correct answer I like shawarma');
       score++ ;
   }
   else
@@ -43,7 +50,8 @@ if  (Q2 === 'yes' || Q2==='y')
     console.log('Incorrect answer I like shawarma');
     alert ('Incorrect answer I like shawarma');
   }
-
+}
+function myChildren(){
   let Q4 = prompt ('Do I Have Children?').toLocaleLowerCase()
   if  (Q4=== 'yes' || Q4==='y')
   {
@@ -56,7 +64,9 @@ if  (Q2 === 'yes' || Q2==='y')
     console.log('Incorrect answer I have children');
     alert ('Incorrect answer I have children');
   }
+}
 
+function myCar(){
   let Q5 = prompt ('Do I have a car?').toLocaleLowerCase()
   if  (Q5=== 'yes' || Q5==='y')
   {
@@ -69,8 +79,10 @@ if  (Q2 === 'yes' || Q2==='y')
     alert ('correct answer I havent car');
     score++ ;
   }
+}
 
   // Seconde Part
+  function myGuess(){
   let Q6 = Number(prompt ('Guess My Age Between 20 & 40'));
  
   if(Q6 === 30)
@@ -116,9 +128,11 @@ if  (Q2 === 'yes' || Q2==='y')
       }       
      
 
-   
+    }
 
    // Third Part
+
+   function muNumber(){
    let myNum = [10 , 4 , 12];
    alert ('Now I have Three Numbers in My mind');
    let Q7= Number(prompt ('Guess One Number From Three Numbers from 0 to 20?'));
@@ -154,8 +168,15 @@ if  (Q2 === 'yes' || Q2==='y')
                
     }
      
-   
-    
+  }   
+  
+  myCar();
+  myBike();
+  myChildren();
+  myLaptop();
+  myShawrma();
+  myGuess();
+  muNumber();  
  alert('Your score is: '+score+' out of 7');
  alert ('Thank  you  ' + name +  ' for Answering And Welcome ');
 
